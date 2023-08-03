c8103b021ba20da84bbfce69f21465483217f0d3
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-08-03 15:59:31 -0500

    lol

---
+++ flask/fundamentals/templates-demo/server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """
+    this is the root route
+    """
+
+    name = "Austin"
+
+    return render_template("index.html", name=name)
+
+
+@app.route("/<name>")
+def name(person):
+    """
+    This is a dynamic route that displays the name passed into the URL
+    """
+
+    return render_template("index.html", name=person)
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=5001)
