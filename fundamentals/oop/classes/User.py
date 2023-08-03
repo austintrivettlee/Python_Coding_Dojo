36dc2001031b9026703e6e07f8aab18d24a2cb7e
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-08-03 10:01:01 -0500

    update

---
+++ fundamentals/oop/classes/User.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self):
+        self.first_name = "Ada"
+        self.last_name = "Lovelace"
+        self.age = 42
+
+
+
+user_ada = User()
+
+print(user_ada.first_name)
+
+user2 = User()
+print(user2.first_name)
